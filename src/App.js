32a75165d7445755eda9599f7b4218828d99ff14
thienthuc9 <phandinhthethien@gmail.com>
2020-08-26 21:33:05 +0700

    BTLayout

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/BTLayout/Header';
+import SideBar from './Components/BTLayout/SideBar';
+import BtLayout from './Components/BTLayout/BtLayout';
+
+function App() {
+  return (
+    <div className="App">
+     <BtLayout></BtLayout>
+    </div>
+  );
+}
+
+export default App;
